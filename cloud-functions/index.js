b3878874f0b8d9afaa0afb81d54a44e685573dd5
shibamoto <silverbirder@gmail.com>
2021-06-19 18:04:25 +0900

    🙂 add sample firestore code

--- cloud-functions/index.js
+++ cloud-functions/index.js
@@ -1,37 +1,38 @@
-const {Firestore} = require('@google-cloud/firestore');
+// const {Firestore} = require('@google-cloud/firestore');
 
 // Create a new client
-const firestore = new Firestore();
+// const firestore = new Firestore();
 
 async function quickstart() {
+  // let documentRef = firestore.collection('tweet').doc();
+
+  // documentRef.create({foo: 'bar'}).then((res) => {
+  //   console.log(`Document created at ${res.updateTime}`);
+  // }).catch((err) => {
+  //   console.log(`Failed to create document: ${err}`);
+  // });
   // Obtain a document reference.
-  const document = firestore.doc('posts/intro-to-firestore');
+  // const document = firestore.doc('tweet/a');
 
-  // Enter new data into the document.
-  await document.set({
-    title: 'Welcome to Firestore',
-    body: 'Hello World',
-  });
-  console.log('Entered new data into the document');
+  // // Enter new data into the document.
+  // await document.set({
+  //   title: 'Welcome to Firestore',
+  //   body: 'Hello World',
+  // });
+  // console.log('Entered new data into the document');
 
-  // Update an existing document.
-  await document.update({
-    body: 'My first Firestore app',
-  });
-  console.log('Updated an existing document');
+  // // Update an existing document.
+  // await document.update({
+  //   body: 'My first Firestore app',
+  // });
+  // console.log('Updated an existing document');
 
-  // Read the document.
-  const doc = await document.get();
-  console.log('Read the document');
+  // // Read the document.
+  // const doc = await document.get();
+  // console.log('Read the document');
 
-  // Delete the document.
-  await document.delete();
-  console.log('Deleted the document');
+  // // Delete the document.
+  // await document.delete();
+  // console.log('Deleted the document');
 }
 quickstart();
-
-exports.helloWorld = async (req, res) => {
-  await quickstart();
-  let message = req.query.message || req.body.message || 'Hello World!';
-  res.status(200).send(message);
-};
