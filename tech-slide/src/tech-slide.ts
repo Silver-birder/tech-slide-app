a5852fc03769a77296bfd0d2403a81bf8205cedb
shibamoto <silverbirder@gmail.com>
2021-06-25 20:25:42 +0900

    😎 Refactoring

--- tech-slide/src/tech-slide.ts
+++
@@ -1,5 +0,0 @@
-import { TechSlide } from './TechSlide.js';
-import { EmbedlyCard } from './EmbedlyCard.js';
-
-customElements.define('tech-slide', TechSlide);
-customElements.define('embedly-card', EmbedlyCard);
